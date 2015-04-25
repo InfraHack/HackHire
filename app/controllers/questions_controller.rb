7f15322139a7347f05863956bac670e457ceeea3
jhenrich <jhenrich@constantcontact.com>
2015-04-25 13:01:34 -0400

    Form works for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,22 @@
 class QuestionsController < ApplicationController
-  def show
-    @question = DATA[params[:id].to_i]
+  before_filter :set_question
 
+  def show
+    choices = @question['correct'] + @question['incorrect']
+    choices.shuffle!
+    @question['choices'] = choices
+
+  end
+
+  def set_question
+    @question = DATA[params[:id].to_i]
+    not_found unless @question
+  end
+
+  def answer
+    puts params[:id]
+    @answer = {}
+    @answer['content'] = params['answer']
+    @answer['correct'] = @question['correct'].include? params['answer']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+    post 'answer'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
