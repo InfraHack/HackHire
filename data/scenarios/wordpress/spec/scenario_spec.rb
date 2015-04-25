d871c5e8a4f7011abea2d9f788afe6833cc93e67
Norm MacLennan <norm.maclennan@gmail.com>
2015-04-25 15:50:07 -0400

    adding the missing tests

--- data/scenarios/wordpress/spec/scenario_spec.rb
+++ data/scenarios/wordpress/spec/scenario_spec.rb
@@ -3,3 +3,8 @@ require_relative '../../spec_helper'
 describe port(80) do
   it { should be_listening.with('tcp') }
 end
+
+describe command('curl -X GET http://localhost/xmlrpc.php') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /XML-RPC server accepts POST requests only/ }
+end
